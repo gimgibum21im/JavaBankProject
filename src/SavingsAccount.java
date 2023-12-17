2465bd64f6addb99a027bf793a37c8a037a8febd
TATOGG <gimgibum21@gmail.com>
2023-12-17 18:43:36 +0900

    SavingsAccount doneImplement 추가

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,15 +1,17 @@
 public class SavingsAccount extends BankAccount {
     private int TOTAL_DEPOSIT_COUNT;
     private int currentDepositCount;
+    private boolean doneImplement;
     private final double RATE = 0.4;
 
     // TDC = TOTAL_DEPOSIT_COUNT
     // CDC = currentDepositCount
     public SavingsAccount(boolean newWDS, String newAccountNum, String newAccountHolder, double newAccountBalance,
-            int newTDC, int newCDC) {
+            int newTDC, int newCDC, boolean newDoneImplement) {
         super(newWDS, newAccountNum, newAccountHolder, newAccountBalance);
         TOTAL_DEPOSIT_COUNT = newTDC;
         currentDepositCount = newCDC;
+        doneImplement = newDoneImplement;
     }
 
     public void implement() {
@@ -17,8 +19,10 @@ public class SavingsAccount extends BankAccount {
     }
 
     public boolean compareDepositCount() {
-        if (currentDepositCount == TOTAL_DEPOSIT_COUNT)
+        if (currentDepositCount == TOTAL_DEPOSIT_COUNT && !doneImplement) {
+            doneImplement = true;
             return true;
+        }
         return false;
     }
 }

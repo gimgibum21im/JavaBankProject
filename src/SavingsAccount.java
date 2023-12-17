efdb4f470bd494d90a24be9c969fed23e3efa8b3
TATOGG <gimgibum21@gmail.com>
2023-12-17 18:36:08 +0900

    SavingsAccount implement 작성

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,13 +1,22 @@
 public class SavingsAccount extends BankAccount {
     private int TOTAL_DEPOSIT_COUNT;
     private int currentDepositCount;
+    private final double RATE = 0.4;
 
     // TDC = TOTAL_DEPOSIT_COUNT
     // CDC = currentDepositCount
-    public SavingsAccount(boolean newWDS, String newAccountNum, String newAccountHolder, int newAccountBalance,
+    public SavingsAccount(boolean newWDS, String newAccountNum, String newAccountHolder, double newAccountBalance,
             int newTDC, int newCDC) {
         super(newWDS, newAccountNum, newAccountHolder, newAccountBalance);
         TOTAL_DEPOSIT_COUNT = newTDC;
         currentDepositCount = newCDC;
+    }
+
+    public boolean implement() {
+        if (TOTAL_DEPOSIT_COUNT == currentDepositCount) {
+            accountBalance += getAccountBalance() * (RATE * (double) (TOTAL_DEPOSIT_COUNT / 12));
+            return true;
+        } else
+            return false;
     }
 }
--- src/depositAccount.java
+++ src/depositAccount.java
@@ -1,5 +1,5 @@
 public class DepositAccount extends BankAccount {
-    public DepositAccount(boolean newWDS, String newAccountNum, String newAccountHolder, int newAccountBalance) {
+    public DepositAccount(boolean newWDS, String newAccountNum, String newAccountHolder, double newAccountBalance) {
         super(newWDS, newAccountNum, newAccountHolder, newAccountBalance);
     }
 }
